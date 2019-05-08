e0b1145bf18d4c0d31b1e1974ab586006c0c59fa
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-05-08 11:06:57 +0100

    parrot is tested for baseSpeed value

--- ParrotTests/ParrotTests.swift
+++ ParrotTests/ParrotTests.swift
@@ -2,4 +2,8 @@ import XCTest
 
 class ParrotTests: XCTestCase {
     
+    func testHasBaseSpeedOf12() {
+        let parrot = Parrot(numberOfCoconuts: 0)
+        XCTAssert(parrot.baseSpeed == 12.0)
+    }
 }
